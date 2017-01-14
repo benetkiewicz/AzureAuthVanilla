564973ac0f387dcfd5254eac7bd21e11ebda4f2e
Piotr Benetkiewicz <piotr@ais.pl>
2017-01-14 21:14:12 +0100

    Initial commit - simplest azure B2E auth worrking

---
+++ AzureAuthVanilla/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace AzureAuthVanilla.Controllers
+{
+    using System.Web.Mvc;
+
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content(string.Format("Hello {0}", User.Identity.Name));
+        }
+    }
+}
---
+++ AzureAuthVanilla/Startup.cs
@@ -0,0 +1,32 @@
+﻿[assembly: Microsoft.Owin.OwinStartup(typeof(AzureAuthVanilla.Startup))]
+
+namespace AzureAuthVanilla
+{
+    using System.Web.Configuration;
+    using Microsoft.Owin.Security;
+    using Microsoft.Owin.Security.Cookies;
+    using Microsoft.Owin.Security.OpenIdConnect;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            string authorityUrl = string.Format("https://login.microsoftonline.com/{0}", WebConfigurationManager.AppSettings["ida:Tenant"]);
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = WebConfigurationManager.AppSettings["ida:ClientId"],
+                    Authority = authorityUrl
+                });
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
